772b07a4b896ced30abb903028a52b97306192ce
Alfredo Salazar <>
2019-07-04 15:44:16 -0500

    Fetch call on click

--- src/App.js
+++ src/App.js
@@ -7,28 +7,62 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: [],
-      selected: null
+      books: [{
+        title : "",
+        author : "",
+        publishedDate : ""
+      }],
+      selected: null,
+      baseUrl : "https://www.googleapis.com/books/v1/volumes"
     }
   }
 
-  componentDidMount() {
-    fetch('https://www.googleapis.com/books/v1/volumes?q=henry&key=AIzaSyDivwzMumjxqEjgIiFyx9fmKchqPdW5aZk&filter=partial&printType=books')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
+  onSearchClick = (searchTerm) => {
+    let url = this.state.baseUrl + `?q=${searchTerm}`;
+
+    console.log(url);
+
+    fetch(url)
+      .then(response => {
+        if ( response.ok ){
+          return response.json();
+        }
       })
-  }
+      .then(responseJSON => {
+
+        let listOfbooks = responseJSON.items.map(item => {
+            return {
+                title: item.volumeInfo.title,
+                author: item.volumeInfo.authors[0],
+                publishedDate: item.volumeInfo.publishedDate
+            }
+        });
+
+        this.setState({
+            books : listOfbooks
+        });
+      });
+  };
+
   render() {
-  return (
-    <main className='App'>
-			<header>
-      	<h1 className='googleTitle'>Google Book Search</h1>
-				<SearchForm />
-        <TypeForm />
-			</header>
-    </main>
-  );
+    return (
+      <main className='App'>
+        <header>
+          <h1 className='googleTitle'>Google Book Search</h1>
+          <SearchForm onSearchClick={this.onSearchClick}/>
+          <TypeForm />
+          <div className="results">
+            {this.state.books.map(book => {
+              return (<div>
+                        <h2> {book.title}</h2>
+                        <h3> {book.author}</h3>
+                        <h4> {book.publishedDate}</h4>
+                      </div>)
+            })}
+          </div>
+        </header>
+      </main>
+    );
   }
 }
 
--- src/search-form/search-form.js
+++ src/search-form/search-form.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 
-class searchForm extends Component {
-  render() {
-  return (
-    <section className= 'searchForm'>
-			<form>
-				<fieldset>
-					<label htmlFor= 'searchInput'>Search: </label>
-					<input type='text' id= 'searchInput'/>
-					<button type='submit'>Search</button>
-				</fieldset>
-			</form>
-		</section>
-  );
+class searchForm extends React.Component {
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
+
+		let searchTerm = event.currentTarget.searchInput.value;
+        this.props.onSearchClick(searchTerm);
+	}
+
+	render() {
+	  return (
+		<section className= 'searchForm'>
+				<form onSubmit={this.onFormSubmit}>
+					<fieldset>
+						<label htmlFor= 'searchInput'>Search: </label>
+						<input type='text' id= 'searchInput'/>
+						<button type='submit'>Search</button>
+					</fieldset>
+				</form>
+			</section>
+	  );
   }
 }
 
